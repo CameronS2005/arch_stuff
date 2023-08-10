7a156b441b1e96aa0cf5bbeb6fad6381e38239fd
CameronS2005 <73669402+CameronS2005@users.noreply.github.com>
2023-08-10 05:23:55 -0400

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+## CONFIG
+ROOTCRYPT_ID="rootcrypt"
+
+USERNAME="Archie" # your non-root users name
+HOSTNAME="$USERNAME" # for testing... as idc ab username or hostname
+#HOSTNAME="Archie" # your installs hostname
+
+GRUB_ID="GRUB" # grub entry name
+
+
+## START OF SCRIPT
+
+arch_chroot() {
+	#genfstab -U /mnt >> /mnt/etc/fstab # generate fstab file (magically handles swap lol)
+
+	#arch-chroot /mnt
+	echo "Will be prompted to enter new root password"
+	passwd
+
+	#swapon ""$DRIVE_ID"p2"
+
+	sed -i 's/^#\(en_US.UTF-8 UTF-8\)/\1/' "/etc/locale.gen"
+	locale-gen
+	echo "LANG=en_US.UTF-8" > /etc/locale.conf
+	export "LANG=en_US.UTF-8"
+
+	echo "Setting System Time & Hostname!"
+	ln -sf "/usr/share/zoneinfo/America/New_York" "/etc/localtime"
+	hwclock --systohc --utc # check 2nd argument # why is this utc?
+	echo "$HOSTNAME" > /etc/hostname
+
+	systemctl enable fstrim.timer # ssd trimming? # add check to see if even using ssd
+
+	#sed -i '/^\s*#[multilib]/ s/^#//' "/etc/pacman.conf" # this doesnt work so fix it to automatically allow 32 bit support!
+	#pacman -Sy
+
+	#[multilib]
+	#Include = /etc/pacman.d/mirrorlist
+
+	echo "Configuring Hosts File With Hostname: ($HOSTNAME)!"
+	cat << EOF >> /etc/hosts
+127.0.0.1 		localhost
+::1 			localhost
+127.0.1.1 		$HOSTNAME.localdomain	$HOSTNAME
+EOF
+
+	echo "Creating & Configuring non-root User: ($USERNAME)"
+	useradd -mG wheel $USERNAME # modify user permissions here
+	echo "Will be prompted to enter new password for ($USERNAME)"
+	passwd $USERNAME
+
+	# we shall not automatically tamper with suderos
+	#EDITOR=nano visudo 
+
+	#### HOW TO UNCOMMENT WHEELS LINE IN VISUDO WITHOUT INTERACTING!!!
+	## USER WILL NOT BE IN SUDOERS UNTIL THIS IS FIXED!
+
+	echo "Configuring Bootloader!"
+	#### THIS NEEDS DONE ASAP #########################rewoworioirwirwiroeirewirpoiewriweroiweoriewporiewpriweporieporipoweirpoewir
+	sed -i '/^HOOKS=/ s/)$/ encrypt)/' "/etc/mkinitcpio.conf" # adds encrypt to hooks
+	mkinitcpio -p linux
+
+	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=$GRUB_ID
+	grub-mkconfig -o /boot/grub/grub.cfg
+
+	## UNTESTED! #### NEED TO SET UUID
+	CRYPT_UUID=$(blkid -s UUID -o value "$DRIVE_ID")
+	new_value="cryptdevice=UUID=$CRYPT_UUID:$ROOTCRYPT_ID root=/dev/mapper/$ROOTCRYPT_ID"
+	sed -i "s/^GRUB_CMDLINE_LINUX=\"[^\"]*\"/GRUB_CMDLINE_LINUX=\"$new_value\"/" /etc/default/grub
+	grub-mkconfig -o /boot/grub/grub.cfg
+
+	systemctl enable NetworkManager
+	systemctl enable bluetooth
+}
+arch_chroot
+
+## END OF SCRIPT
