b6455a84f26c25e577bf86f9a6e97797b756018c
CameronS2005 <73669402+CameronS2005@users.noreply.github.com>
2024-07-07 14:04:25 -0400

    Update installer-testing.sh

--- installer-testing.sh
+++ installer-testing.sh
@@ -18,7 +18,7 @@
 
 ###VARIABLES_START
 # Global variables
-rel_date="UPDATE TIME; Jul 06, 6:03 PM EDT (2024)"
+rel_date="UPDATE TIME; Jul 07, 2:04 PM EDT (2024)"
 SCRIPT_VERSION="v1.6"
 ARCH_VERSION="2024.06.01"
 WIFI_SSID="dacrib"
